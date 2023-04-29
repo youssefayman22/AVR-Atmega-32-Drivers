b279a12cee6dfd5a3cee485ac75784cf6073bc6a
youssefayman22 <103893221+youssefayman22@users.noreply.github.com>
2023-04-29 13:10:41 +0200

    Delete BITMATH.h

--- BITMATH.h
+++
@@ -1,6 +0,0 @@
-#include "STD_TYPES.h"
-
-#define		SetBit(REG,BIT)		 (REG |= (1<<BIT));
-#define		ClrBit(REG,BIT)		 (REG &= ~(1<<BIT));
-#define		ToggleBit(REG,BIT)	 (REG ^= (1<<BIT));
-#define		GetBit(REG,BIT)		 (REG = (REG>>BIT)&1);
